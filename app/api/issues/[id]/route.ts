6f1c596fdbb30918be5e0ef0150a9bbcebd1dae6
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-01-15 23:50:23 +0100

    Update issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,6 @@
 import { issueSchema } from "@/app/validationSchemas";
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, {params}: {params: {id: string}}) {
     const body = await request.json();
