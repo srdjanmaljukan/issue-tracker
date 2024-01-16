77055849b025290f98c325603e40770d4b1ad99e
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-01-16 11:25:04 +0100

    Improve loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import IssueForm from '../../_components/IssueForm'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
+import prisma from "@/prisma/client";
+import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
-  params: {id: string}
+  params: { id: string };
 }
 
-const EditIssuePage = async ({params}: Props) => {
+const EditIssuePage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
-  })
+    where: { id: parseInt(params.id) },
+  });
 
-  if (!issue) notFound()
+  if (!issue) notFound();
 
-  return (
-    <IssueForm issue={issue} />
-  )
-}
+  return <IssueForm issue={issue} />;
+};
 
-export default EditIssuePage
+export default EditIssuePage;
