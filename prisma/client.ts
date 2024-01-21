4c55e119edae8d37c9b372cbb2d05348b587c3ef
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-01-21 23:59:18 +0100

    Make home page dynamic

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({log: ['query']})
+  return new PrismaClient()
 }
 
 declare global {
